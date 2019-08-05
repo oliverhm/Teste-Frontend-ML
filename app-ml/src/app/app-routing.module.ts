c2cb9b1334689b37abfb0eae886926d936396844
Oliver Henrique Maceira <oliver@ciandt.com>
2019-08-05 19:38:01 -0300

    Ajustes gerais de dependência.

--- app-ml/src/app/app-routing.module.ts
+++ app-ml/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ResultsComponent } from './results/results.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'items', component: ResultsComponent },
+  { path: 'items/:id', component: ProductDetailsComponent },
+  { path: '**', component: ResultsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app-ml/src/app/app.module.ts
+++ app-ml/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ResultsComponent } from './results/results.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ResultsService } from './results/results.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ResultsComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ResultsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

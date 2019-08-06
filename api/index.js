2427659141889075af15b888387c5851cf6296fd
Oliver Henrique Maceira <oliver@ciandt.com>
2019-08-06 16:39:30 -0300

    Usa try catch para as requisições das APIs.

--- api/index.js
+++ api/index.js
@@ -7,88 +7,98 @@ app.use(cors());
 const apiHelper = require('./api-helper');
 const port = 3000;
 
+const API_ML = 'https://api.mercadolibre.com';
+const ENDPOINT_ITEMS = `${API_ML}/items`;
+const ENDPOINT_SITES = `${API_ML}/sites/MLA`;
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.get("/api/items", (req, res) => {
-  const query = req.query.q;
+app.get("/api/items", async (req, res) => {
+  try {
+    const query = req.query.q;
+    const response = await apiHelper.callAPI(`${ENDPOINT_SITES}/search?q=${query}`);
+    const items = [];
 
-  apiHelper.callAPI(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
-    .then(response => {
-      const results = response.results;
-      const items = [];
+    response.results.forEach(element => {
+      const price = formatPrice(element.price);
 
-      results.forEach(element => {
-        const price = formatPrice(element.price);
-
-        items.push({
-          id: element.id,
-          title: element.title,
-          price: {
-            currency: element.currency_id,
-            amount: Number(price[0]),
-            decimals: Number(price[1])
-          },
-          address: {
-            state_id: element.address.state_id,
-            state_name: element.address.state_name,
-          },
-          picture: element.thumbnail,
-          condition: element.condition,
-          free_shipping: element.shipping.free_shipping
-        });
-      });
-
-      res.json({
-        author: {
-          name: "Oliver Henrique",
-          lastname: "Maceira"
+      items.push({
+        id: element.id,
+        title: element.title,
+        price: {
+          currency: element.currency_id,
+          amount: Number(price[0]),
+          decimals: Number(price[1])
         },
-        categories: ['category 1', 'category 2', 'category 3'],
-        items: items
+        address: {
+          state_id: element.address.state_id,
+          state_name: element.address.state_name,
+        },
+        picture: element.thumbnail,
+        condition: element.condition,
+        free_shipping: element.shipping.free_shipping
       });
-    })
-    .catch(error => {
-      res.send(error);
     });
+
+    const data = {
+      author: {
+        name: "First Name",
+        lastname: "Last Name"
+      },
+      categories: ['String', 'String', 'String'],
+      items: items
+    };
+
+    res.status(200).send(data);
+  } catch (err) {
+    genericError(err, res);
+  }
 });
 
-app.get("/api/items/:id", (req, res) => {
+app.get("/api/items/:id", async (req, res) => {
   const id = req.params.id;
 
-  apiHelper.callAPI(encodeURI(`https://api.mercadolibre.com/items/${id}`))
-    .then(response => {
-      const price = formatPrice(response.price);
-
-      res.json({
-        author: {
-          name: "Oliver Henrique",
-          lastname: "Maceira",
+  try {
+    const item = await apiHelper.callAPI(`${ENDPOINT_ITEMS}/${id}`);
+    const price = formatPrice(item.price);
+    const data = {
+      author: {
+        name: "Oliver Henrique",
+        lastname: "Maceira",
+      },
+      item: {
+        id: item.id,
+        title: item.title,
+        price: {
+          currency: item.currency_id,
+          amount: Number(price[0]),
+          decimals: Number(price[1]),
         },
-        item: {
-          id: response.id,
-          title: response.title,
-          price: {
-            currency: response.currency_id,
-            amount: Number(price[0]),
-            decimals: Number(price[1]),
-          },
-          picture: response.pictures[0].secure_url,
-          condition: response.condition,
-          free_shipping: false,
-          sold_quantity: response.sold_quantity,
-          description: 'mimimim'
-        }
-      });
-    })
-    .catch(error => {
-      console.error(error);
-      res.send(error);
-    });
+        picture: item.pictures[0].secure_url,
+        condition: item.condition,
+        free_shipping: false,
+        sold_quantity: item.sold_quantity,
+        description: ''
+      }
+    };
+
+    const description = await apiHelper.callAPI(`${API_ITEMS}/${id}/description`);
+    data.item.description = description.plain_text;
+
+    res.status(200).send(data);
+  } catch (err) {
+    genericError(err, res);
+  }
 });
 
 function formatPrice(price) {
   const priceStr = price.toString();
   return priceStr.split(".");
 };
+
+function genericError(err, res) {
+  console.error(err);
+  res.status(500).send(err);
+}
